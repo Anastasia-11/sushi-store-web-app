00a5f636109a106158653a9312fc3395e14814cd
Anastasiya Fedorovich <anastasiaf0311@yandex.by>
2021-12-09 01:29:35 +0300

    add roles

---
+++ CourseProject/CourseProject/Controllers/RolesController.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CourseProject.Models;
+using CourseProject.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseProject.Controllers
+{
+    public class RolesController : Controller
+    {
+        RoleManager<IdentityRole> _roleManager;
+        UserManager<User> _userManager;
+        
+        public RolesController(RoleManager<IdentityRole> roleManager, UserManager<User> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+        public IActionResult Index() => View(_roleManager.Roles.ToList());
+ 
+        public IActionResult Create() => View();
+        
+        [HttpPost]
+        public async Task<IActionResult> Create(string name)
+        {
+            if (!string.IsNullOrEmpty(name))
+            {
+                IdentityResult result = await _roleManager.CreateAsync(new IdentityRole(name));
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                AddModelErrors(result);
+            }
+            return View(name);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(RoleModificationViewModel model)
+        {
+            IdentityResult result;
+            if (ModelState.IsValid)
+            {
+                foreach (string userId in model.IdsToAdd ?? new string[] { })
+                {
+                    User user = await _userManager.FindByIdAsync(userId);
+                    if (user != null)
+                    {
+                        result = await _userManager.AddToRoleAsync(user, model.RoleName);
+                        if (!result.Succeeded)
+                            AddModelErrors(result);
+                    }
+                }
+
+                foreach (string userId in model.IdsToDelete ?? new string[] { })
+                {
+                    User user = await _userManager.FindByIdAsync(userId);
+                    if (user != null)
+                    {
+                        result = await _userManager.RemoveFromRoleAsync(user, model.RoleName);
+                        if (!result.Succeeded)
+                            AddModelErrors(result);
+                    }
+                }
+            }
+            if (ModelState.IsValid)
+                return RedirectToAction(nameof(Index));
+            return await Edit(model.RoleId);
+        }
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            IdentityRole role = await _roleManager.FindByIdAsync(id);
+            List<User> members = new List<User>();
+            List<User> nonMembers = new List<User>();
+            foreach (User user in _userManager.Users.ToList())
+            {
+                if (await _userManager.IsInRoleAsync(user, role.Name))
+                {
+                    members.Add(user);
+                }
+                else
+                {
+                    nonMembers.Add(user);
+                }
+            }
+
+            return View(new EditRoleViewModel
+            {
+                Role = role,
+                Members = members,
+                NonMembers = nonMembers
+            });
+        }
+
+        public async Task<IActionResult> Delete(string id)
+        {
+            IdentityRole role = await _roleManager.FindByIdAsync(id);
+            await _roleManager.DeleteAsync(role);
+            return RedirectToAction("Index", "Roles");
+        }
+
+        private void AddModelErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+        }
+    }
+}
---
+++ CourseProject/CourseProject/ViewModels/EditRoleViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using CourseProject.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace CourseProject.ViewModels
+{
+    public class EditRoleViewModel
+    {
+        public IdentityRole Role { get; set; }
+        public IEnumerable<User> Members { get; set; }
+        public IEnumerable<User> NonMembers { get; set; }
+    }
+}
